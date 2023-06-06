db973af36e3171921745017e24c2252ed917a403
«GutnikAnna» <gutnikanna1@gmail.com>
2023-06-06 17:15:11 +0700

    Вторая лекция

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 Random random = new Random();
 int randomNumber = random.Next(10, 100);
 
@@ -14,3 +14,82 @@ else
 {
     Console.WriteLine(lastDigit); 
 }
+*/
+
+
+
+/*
+Random random = new Random();
+int randomN = random.Next(100, 1000);
+
+int last = randomN % 10;
+
+int first = randomN / 100;
+
+Console.WriteLine(randomN);
+
+/*Console.Write(first);
+Console.Write(last);//
+
+Console.WriteLine(first*10 + last);*/
+
+/*
+Console.WriteLine("Введите два числа");
+
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+
+int R = first % second;
+
+if (R == 0)
+{
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    /*Console.WriteLine("Не кратно - остаток " + R); = //
+    Console.WriteLine($"Не кратно - остаток {R}");
+}
+*/
+
+
+
+
+/*
+Console.WriteLine("Введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+if(N % 7 == 0 & N % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
+*/
+
+
+
+
+
+/*
+Console.WriteLine("Введите два числа");
+
+int F = int.Parse(Console.ReadLine());
+int S = int.Parse(Console.ReadLine());
+
+if(F*F == S | S*S == F)
+{
+   Console.WriteLine("Да");
+ 
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
+*/
